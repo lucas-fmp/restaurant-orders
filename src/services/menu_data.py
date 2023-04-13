1a9f3ff1a03723ea81eb78056334bdeb05a8b956
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-04-13 17:57:49 -0300

    feat - requirement 3 - implement class MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
 # Req 3
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self._load_menu_data(source_path)
+
+    def _load_menu_data(self, source_path):
+        with open(source_path) as menu_data:
+            menu = csv.reader(menu_data)
+            next(menu)
+            for row in menu:
+                dish_name, dish_price, ingredient_name, ingredient_qty = row
+                ingredient = Ingredient(ingredient_name)
+                dish = next(
+                    (dish for dish in self.dishes if dish.name == dish_name),
+                    None,
+                )
+                if dish is None:
+                    dish = Dish(dish_name, float(dish_price))
+                    self.dishes.add(dish)
+                dish.add_ingredient_dependency(ingredient, int(ingredient_qty))
