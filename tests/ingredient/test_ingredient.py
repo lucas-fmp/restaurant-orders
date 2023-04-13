bae2a1e6df9311476662a6b2a42941602577acb7
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-04-13 16:22:09 -0300

    test - requirement 1 - test class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,26 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("queijo mussarela")
+    same_ingredient = Ingredient("queijo mussarela")
+    another_ingredient = Ingredient("farinha")
+
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
+
+    assert ingredient == same_ingredient
+    assert ingredient != another_ingredient
+
+    assert hash(ingredient) == hash(same_ingredient)
+    assert hash(ingredient) != hash(another_ingredient)
+
+    assert ingredient.name == "queijo mussarela"
